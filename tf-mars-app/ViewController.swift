a0a87911e0392abee5b014d3044678979c83d014
Hoang Duc Nguyen <hd.ngy@hoangs-macbook-pro.local>
2019-03-01 20:56:13 +0100

    Initial Commit

---
+++ tf-mars-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tf-mars-app
+//
+//  Created by Hoang Duc Nguyen on 01.03.19.
+//  Copyright © 2019 Hoang Duc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
