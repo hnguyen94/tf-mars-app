40337cd9f258d3fbdd18bab46a5055276dd02668
Hoang Duc Nguyen <hd.ngy@Hoangs-MBP.fritz.box>
2019-03-02 19:51:53 +0100

    init

--- tf-mars-app/ViewController.swift
+++ tf-mars-app/ViewController.swift
@@ -1,20 +1,65 @@
-//
-//  ViewController.swift
-//  tf-mars-app
-//
-//  Created by Hoang Duc Nguyen on 01.03.19.
-//  Copyright © 2019 Hoang Duc Nguyen. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    struct Layout {
+        struct Padding {
+            static let nextGenButtonBottom: CGFloat = 64
+            static let titleLabelTop: CGFloat = 24
+        }
+    }
+    
+    // MARK: - Properties
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Terra Forming Board"
+        label.font = UIFont.systemFont(ofSize: 24)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
+    lazy var nextGenButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Next Generation", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        
+        setupViews()
     }
-
-
+    
+    private func setupViews() {
+        view.addSubview(titleLabel)
+        view.addSubview(nextGenButton)
+        
+        layoutNextGenButton()
+        layoutTitleLabel()
+    }
+    
+    private func layoutTitleLabel() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                            constant: Layout.Padding.titleLabelTop)
+        ])
+    }
+    
+    private func layoutNextGenButton() {
+        NSLayoutConstraint.activate([
+            nextGenButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextGenButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                                  constant: -Layout.Padding.nextGenButtonBottom)
+        ])
+    }
+    
 }
 
