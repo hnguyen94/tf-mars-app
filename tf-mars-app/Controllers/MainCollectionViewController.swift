7e0800a7a7170b98990c9bfe1e7add5aad06580f
Hoang Nguyen <hoang.nguyen@xing.com>
2019-03-29 15:14:33 +0100

    Extract view

--- tf-mars-app/Controllers/MainCollectionViewController.swift
+++ tf-mars-app/Controllers/MainCollectionViewController.swift
@@ -4,72 +4,12 @@ let customCellIdentifier = "tfmCellId"
 
 class MainCollectionViewController: UIViewController {
     
-    // MARK: - Constants
-    
-    struct Layout {
-        struct Padding {
-            static let standard: CGFloat = 8
-            static let standard16: CGFloat = Layout.Padding.standard * 2
-            static let standard24: CGFloat = Layout.Padding.standard * 3
-            static let standard32: CGFloat = Layout.Padding.standard * 4
-            static let standard40: CGFloat = Layout.Padding.standard * 5
-            static let standard48: CGFloat = Layout.Padding.standard * 6
-            static let standard56: CGFloat = Layout.Padding.standard * 7
-            static let standard64: CGFloat = Layout.Padding.standard * 8
-        }
-    }
-    
     // MARK: - Properties
 
-    weak var collectionView: UICollectionView!
     private let viewModel = TFMPropertyContainer()
-    let tfmDatasource: TfmPropertyDataSource
+    private let tfmDatasource: TfmPropertyDataSource
+    private let mainView = MainView()
 
-    // MARK: - View Properties
-    
-    lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Terra Forming Board"
-        label.font = UIFont.systemFont(ofSize: 24)
-        label.textColor = .white
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    lazy var stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = Layout.Padding.standard
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-
-    lazy var scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        return scrollView
-    }()
-    
-    lazy var nextGenBackgroundView: UIView = {
-        let aView = UIView()
-        aView.backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.2431372549, blue: 0.3137254902, alpha: 1)
-        
-        aView.translatesAutoresizingMaskIntoConstraints = false
-        return aView
-    }()
-
-    lazy var nextGenButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Next Generation", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.addTarget(self, action: #selector(nextGeneration), for: .touchUpInside)
-        
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
 
     // MARK: - Init
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -89,79 +29,23 @@ class MainCollectionViewController: UIViewController {
     override func loadView() {
         super.loadView()
 
-
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(collectionView)
-
-        self.collectionView = collectionView
+        view = mainView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.2431372549, blue: 0.3137254902, alpha: 1)
-
         setupCollectionView()
-        setupViews()
-    }
 
-    // MARK: - Constraints
-    
-    /// Setup all views with its constraints
-    private func setupViews() {
-        addSubViews()
-        setupConstraints()
+        mainView.nextGenButton.addTarget(self, action: #selector(nextGeneration), for: .touchUpInside)
     }
-    
-    /// A function for adding subviews.
-    fileprivate func addSubViews() {
-        view.addSubview(titleLabel)
-        view.addSubview(nextGenButton)
-    }
-    
-    /// A function for setting the constraints.
-    fileprivate func setupConstraints() {
-        // Variables
-        let stackLeadingTrailingMargin = Layout.Padding.standard24
-        
-        NSLayoutConstraint.activate([
-            // TitleLabel
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
-                                            constant: Layout.Padding.standard24),
-            
-            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,
-                                               constant: stackLeadingTrailingMargin),
-            
-            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,
-                                                constant: -stackLeadingTrailingMargin),
-            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: Layout.Padding.standard24),
-            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-
-            // NextGenButton
-            nextGenButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            nextGenButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
-                                                  constant: -Layout.Padding.standard)
-            
-            // Next Gen Backgroundview
-//            nextGenBackgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            nextGenBackgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-//            nextGenBackgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-//            nextGenBackgroundView.heightAnchor.constraint(equalToConstant: 100)
-        ])
-    }
-
 
     // MARK: - Methods
 
     private func setupCollectionView() {
-        collectionView.backgroundColor = UIColor.clear.withAlphaComponent(0)
-        collectionView.alwaysBounceVertical = true
-        collectionView.delegate = self
-        collectionView.dataSource = tfmDatasource
+        mainView.collectionView.delegate = self
+        mainView.collectionView.dataSource = tfmDatasource
         tfmDatasource.tfmProperties = viewModel.tfmProperties
-        collectionView.register(TFMPropertyCell.self, forCellWithReuseIdentifier: customCellIdentifier)
     }
 
 }
@@ -179,7 +63,7 @@ extension MainCollectionViewController: UICollectionViewDelegateFlowLayout {
     }
 }
 
-/// Action
+/// Actions
 extension MainCollectionViewController {
     enum Unit {
         case productionFactor
@@ -208,7 +92,7 @@ extension MainCollectionViewController {
         }
 
         UIView.performWithoutAnimation {
-            collectionView.reloadItems(at: [indexPath])
+            mainView.collectionView.reloadItems(at: [indexPath])
         }
     }
 
@@ -217,7 +101,8 @@ extension MainCollectionViewController {
         tfmDatasource.tfmProperties = nextGenerationProperties
 
         DispatchQueue.main.async { [weak self] in
-            self?.collectionView.reloadData()
+            self?.mainView.collectionView.reloadData()
         }
     }
+
 }
---
+++ tf-mars-app/Views/MainView.swift
@@ -0,0 +1,95 @@
+import UIKit
+
+class MainView: UIView {
+
+    struct Layout {
+        struct Padding {
+            static let standard: CGFloat = 8
+            static let standard24: CGFloat = Layout.Padding.standard * 3
+        }
+    }
+
+    // MARK: - Properties
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Terra Forming Board"
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.textColor = .white
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        return label
+    }()
+
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.backgroundColor = UIColor.clear.withAlphaComponent(0)
+        collectionView.alwaysBounceVertical = true
+
+
+        collectionView.register(TFMPropertyCell.self, forCellWithReuseIdentifier: customCellIdentifier)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        return collectionView
+    }()
+
+    lazy var nextGenButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Next Generation", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.2431372549, blue: 0.3137254902, alpha: 1)
+        setupViews()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Constraints
+
+    /// Setup all views with its constraints
+    private func setupViews() {
+        addSubviews()
+        setupConstraints()
+    }
+
+    private func addSubviews() {
+        addSubview(titleLabel)
+        addSubview(collectionView)
+        addSubview(nextGenButton)
+    }
+
+    /// A function for setting the constraints.
+    private func setupConstraints() {
+        let stackLeadingTrailingMargin = Layout.Padding.standard24
+
+        NSLayoutConstraint.activate([
+            // Title Label
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor,
+                                            constant: Layout.Padding.standard24),
+
+            // Collection View
+            collectionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor,
+                                                    constant: stackLeadingTrailingMargin),
+            collectionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor,
+                                                     constant: -stackLeadingTrailingMargin),
+            collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: Layout.Padding.standard24),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            // Next Gen Button
+            nextGenButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            nextGenButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor,
+                                                  constant: -Layout.Padding.standard)
+        ])
+    }
+}
