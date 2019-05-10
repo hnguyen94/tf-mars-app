c8eaea5cebba161a0873eb1a8021b2d43f325387
Hoang Nguyen <hoang.nguyen@xing.com>
2019-05-10 17:37:21 +0200

    Change UI

--- tf-mars-app/Datasources Delegates/MainCollectionViewDelegate.swift
+++ tf-mars-app/Datasources Delegates/MainCollectionViewDelegate.swift
@@ -1,21 +1,22 @@
 import UIKit
 
 class MainCollectionViewDelegate: NSObject, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let padding: CGFloat = 24
-        let cellSize = collectionView.frame.size.width - padding
-        let size = CGSize(width: cellSize/2, height: 232)
-        return size
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        referenceSizeForFooterInSection section: Int) -> CGSize {
-        let size = CGSize(width: collectionView.frame.width, height: 110)
-        return size
-    }
-    
+
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let padding: CGFloat = 24
+    let height: CGFloat = 196
+    let cellSize = collectionView.frame.size.width - padding
+    let size = CGSize(width: cellSize/2, height: height)
+    return size
+  }
+
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      referenceSizeForFooterInSection section: Int) -> CGSize {
+    let size = CGSize(width: collectionView.frame.width, height: 110)
+    return size
+  }
+
 }
--- tf-mars-app/Views/Cells/TFMPropertyCell.swift
+++ tf-mars-app/Views/Cells/TFMPropertyCell.swift
@@ -7,59 +7,85 @@ class TFMPropertyCell: UICollectionViewCell {
   struct Layout {
     struct Cell {
       static let cornerRadius: CGFloat = 10
+      static let borderWidth: CGFloat = 1
     }
     
     struct Padding {
       static let standard: CGFloat = 8
       static let standard16: CGFloat = Layout.Padding.standard * 2
+      static let standard24: CGFloat = Layout.Padding.standard * 3
+    }
+
+    struct Button {
+      static let borderWidth: CGFloat = 1
+      static let cornerRadius: CGFloat = 8
+      static let horizontalSpaceProduction: CGFloat = 12
+      static let horizontalSpaceQuantity: CGFloat = 18
+      static let verticalSpace: CGFloat = 3
     }
   }
-  
+
   struct FontSize {
-    static let big: CGFloat = 18
-    static let normal: CGFloat = 16
+    static let title: CGFloat = 12
+    static let property: CGFloat = 10
   }
   
   // MARK: - Properties
   
   var model: TFMPropertyModel? {
     didSet {
-      setContent()
-      setupActions()
+      setupData()
     }
   }
   
   // MARK: - View Properties
 
-//  lazy var icon: UIImageView = {
-//    let icon = UIImageView()
-//  }()
-  
+  lazy var iconView: UIImageView = {
+    let icon = UIImageView()
+    icon.translatesAutoresizingMaskIntoConstraints = false
+    return icon
+  }()
+
   lazy var titleLabel: UILabel = {
     let label = UILabel()
     label.textColor = .white
-    label.font = UIFont.systemFont(ofSize: FontSize.big, weight: .bold)
+    label.font = UIFont.systemFont(ofSize: FontSize.title, weight: .bold)
     label.translatesAutoresizingMaskIntoConstraints = false
     return label
   }()
   
-  lazy var productionFactorLabel: UILabel = {
-    let label = UILabel()
-    label.textColor = .white
-    label.font = UIFont.systemFont(ofSize: FontSize.normal)
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
+  lazy var productionFactorButton: UIButton = {
+    let button = UIButton()
+    button.setTitleColor(UIColor.TFMOrange.light, for: .normal)
+    button.titleLabel?.font = UIFont.systemFont(ofSize: FontSize.property)
+    button.backgroundColor = .clear
+    button.layer.cornerRadius = Layout.Button.cornerRadius
+    button.layer.borderWidth = Layout.Button.borderWidth
+    button.layer.borderColor = UIColor.TFMOrange.light.cgColor
+    button.contentEdgeInsets = UIEdgeInsets(top: Layout.Button.verticalSpace,
+                                            left: Layout.Button.horizontalSpaceProduction,
+                                            bottom: Layout.Button.verticalSpace,
+                                            right: Layout.Button.horizontalSpaceProduction)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    return button
   }()
-  
-  lazy var quantityLabel: UILabel = {
-    let label = UILabel()
-    label.textColor = .white
-    label.font = UIFont.systemFont(ofSize: FontSize.normal)
-    label.font = UIFont.systemFont(ofSize: FontSize.normal)
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
+
+  lazy var quantityButton: UIButton = {
+    let button = UIButton()
+    button.setTitleColor(UIColor.TFMOrange.light, for: .normal)
+    button.titleLabel?.font = UIFont.systemFont(ofSize: FontSize.property)
+    button.backgroundColor = .clear
+    button.layer.cornerRadius = Layout.Button.cornerRadius
+    button.layer.borderWidth = Layout.Button.borderWidth
+    button.layer.borderColor = UIColor.TFMOrange.light.cgColor
+    button.contentEdgeInsets = UIEdgeInsets(top: Layout.Button.verticalSpace,
+                                            left: Layout.Button.horizontalSpaceQuantity,
+                                            bottom: Layout.Button.verticalSpace,
+                                            right: Layout.Button.horizontalSpaceQuantity)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    return button
   }()
-  
+
   lazy var productionStepper: UIStepper = {
     let stepper = UIStepper()
     stepper.wraps = true
@@ -80,7 +106,7 @@ class TFMPropertyCell: UICollectionViewCell {
   // MARK: - Init
   override init(frame: CGRect) {
     super.init(frame: frame)
-    
+
     configureView()
     setupViews()
   }
@@ -93,26 +119,35 @@ class TFMPropertyCell: UICollectionViewCell {
   
   fileprivate func setupConstraints() {
     NSLayoutConstraint.activate([
+      // Icon
+      iconView.centerXAnchor.constraint(equalTo: centerXAnchor),
+      iconView.topAnchor.constraint(equalTo: topAnchor,
+                                    constant: Layout.Padding.standard24),
+
       // TitleLabel
-      titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Layout.Padding.standard16),
-      titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: Layout.Padding.standard),
-      
+      titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+      titleLabel.topAnchor.constraint(equalTo: iconView.bottomAnchor,
+                                      constant: Layout.Padding.standard),
+
+
       // ProductonFactorLabel
-      productionFactorLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
-      productionFactorLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: Layout.Padding.standard),
-      
+      productionFactorButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+      productionFactorButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor,
+                                                  constant: Layout.Padding.standard),
+
       // QuantityLabel
-      quantityLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
-      quantityLabel.topAnchor.constraint(equalTo: productionFactorLabel.bottomAnchor, constant: Layout.Padding.standard16),
-      
+      quantityButton.centerXAnchor.constraint(equalTo: titleLabel.centerXAnchor),
+      quantityButton.topAnchor.constraint(equalTo: productionFactorButton.bottomAnchor,
+                                          constant: CGFloat(6)),
+
       // ProductionStepper
-      productionStepper.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Layout.Padding.standard16),
-      productionStepper.centerYAnchor.constraint(equalTo: productionFactorLabel.centerYAnchor),
-      
+//      productionStepper.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Layout.Padding.standard16),
+//      productionStepper.centerYAnchor.constraint(equalTo: productionFactorLabel.centerYAnchor),
+
       // Quantity Stepper
-      quantityStepper.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Layout.Padding.standard16),
-      quantityStepper.centerYAnchor.constraint(equalTo: quantityLabel.centerYAnchor)
-      
+//      quantityStepper.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Layout.Padding.standard16),
+//      quantityStepper.centerYAnchor.constraint(equalTo: quantityLabel.centerYAnchor)
+
       ])
   }
   
@@ -120,31 +155,45 @@ class TFMPropertyCell: UICollectionViewCell {
   
   /// Styling for the view itself
   private func configureView() {
+    // Border radious
     layer.cornerRadius = Layout.Cell.cornerRadius
     layer.masksToBounds = true
+
+    // Border line
+    layer.borderColor = UIColor.TFMOrange.light.cgColor
+    layer.borderWidth = Layout.Cell.borderWidth
+
   }
   
   private func setupViews() {
     addSubviews(titleLabel,
-                quantityLabel,
-                productionFactorLabel,
-                productionStepper,
-                quantityStepper)
+                iconView,
+                quantityButton,
+                productionFactorButton)
 
     setupConstraints()
   }
-  
-  private func setContent() {
-    titleLabel.text = model?.type.rawValue
-    productionFactorLabel.text = "Production: \(model?.productionFactor ?? 0)"
-    quantityLabel.text = "Quantity: \(model?.quantity ?? 0)"
-    productionStepper.minimumValue = model?.minimumProductionNumber ?? 0
-    productionStepper.maximumValue = model?.maximumProductionNumber ?? 0
+
+  private func setupData() {
+    guard let model = model else {
+      assertionFailure("Model is not set")
+      return
+    }
+    
+    setContent(with: model)
+    setupActions(with: model)
   }
   
-  private func setupActions() {
-    guard let model = model else { return }
-    
+  private func setContent(with model: TFMPropertyModel) {
+    titleLabel.text = model.type.rawValue
+    iconView.image = model.icon
+    productionFactorButton.setTitle("Production: \(model.productionFactor)", for: .normal)
+    quantityButton.setTitle("Quantity: \(model.quantity)", for: .normal)
+    productionStepper.minimumValue = model.minimumProductionNumber
+    productionStepper.maximumValue = model.maximumProductionNumber
+  }
+  
+  private func setupActions(with model: TFMPropertyModel) {
     productionStepper.value = Double(model.productionFactor)
     quantityStepper.value = Double(model.quantity)
     
